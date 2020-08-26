9c97795c7018cf34057928d6ebe727aed45f1329
PqtatoDev1 <gitlab@pqtato.ml>
2020-08-26 17:26:23 -0500

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// web
 const express = require('express');
 const app = express();
 
@@ -7,20 +6,3 @@ app.get("/", (req, res) => {
 });
 
 app.listen(process.env.PORT);
-
-// bot
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const token = process.env.TOKEN;
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
-});
-
-client.login(token);
