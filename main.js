d086a58381aa22a3587c6f3d8e28f9998d309603
PqtatoDev1 <gitlab@pqtato.ml>
2020-08-26 16:58:45 -0500

    before adding actual bot use discord.js example bot

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-//Keep project on 24/7 
-const http = require('http');
+// web
 const express = require('express');
 const app = express();
 
@@ -8,3 +7,20 @@ app.get("/", (req, res) => {
 });
 
 app.listen(process.env.PORT);
+
+// bot
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = process.env.TOKEN;
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
+});
+
+client.login(token);
